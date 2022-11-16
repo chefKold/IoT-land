a0c1e31f0c4adf2e04e545697510ef98bd41920e
chefKold <j.koldenhof@outlook.com>
2022-11-16 15:11:37 +0100

    Remaining setup files

---
+++ apps/iot-land/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'iot-land-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'iotland';
+}
