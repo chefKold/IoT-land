0c08ea0c0586554b2cdae7e371579099460f1c47
chefKold <j.koldenhof@outlook.com>
2022-11-18 15:12:50 +0100

    Added basic navbar

--- apps/iot-land/src/app/app.module.ts
+++ apps/iot-land/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, NavbarComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
