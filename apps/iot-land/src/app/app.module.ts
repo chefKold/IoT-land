d0f617ebbdc12374b690f2b9856058cd514cd629
chefKold <j.koldenhof@outlook.com>
2022-11-18 18:11:11 +0100

    CI/CD setup

--- apps/iot-land/src/app/app.module.ts
+++ apps/iot-land/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
+
 import { NavbarComponent } from "./shared/navbar/navbar.component";
 import { HomeComponent } from "./pages/home/home.component";
 import { AboutComponent } from "./pages/about/about.component";
